f787c6348a7d335d77844bd493110712851a79b9
MattiaCallegaro <mattiacallegarocioi@gmail.com>
2025-03-17 15:02:01 +0100

    [UPD] Add console.log + alert

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 // Chiedere all'utente i chilometri e l'etá
 const km = prompt ("Inserisci il numero di km che vuoi percorrere")
 const age = prompt ("Inserisci la tua etá")
+
+//Calcolare il prezzo del biglietto
+
+const priceForKm = 0.21;
+let ticketPrice = km * priceForKm;
+
+console.log(`Il prezzo del biglietto é : ${ticketPrice} €`)
+alert(`Il prezzo del biglietto é : ${ticketPrice} €`)
